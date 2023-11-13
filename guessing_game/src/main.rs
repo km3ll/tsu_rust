d844513b21850b17555d0729598e37160a8bef00
kmell <kmell@protonmail.com>
2023-11-13 15:11:52 -0500

    Pod 153: Synchronize pod data

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,25 +7,36 @@ fn main() {
 
     let secret: u32 = rand::thread_rng()
         .gen_range(1..=100);
-
     println!("[Guess the number]");
-    println!("042: Please input your guess:");
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("042: Failed to read line");
+    loop {
 
-    let guess: u32 = guess
-        .trim().parse()
-        .expect("042: Please type a number");
+        println!("042: Please input your guess:");
 
-    println!("042: You guessed: {guess}");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("042: Failed to read line");
 
-    match guess.cmp(&secret) {
-        Ordering::Less => println!("042: Too small"),
-        Ordering::Greater => println!("042: Too big"),
-        Ordering::Equal => println!("[You win!]")
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(error) => {
+                println!("Guess cannot be converted: {}", error);
+                continue;
+            }
+        };
+
+        println!("042: You guessed: {guess}");
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("042: Too small"),
+            Ordering::Greater => println!("042: Too big"),
+            Ordering::Equal => {
+                println!("[You win!]");
+                break;
+            }
+        }
+
     }
 
 }
