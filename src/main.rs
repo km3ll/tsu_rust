eb3667a8b86cc03fd756e9d49e85a7eb3c6da4fe
kmell <kmell@protonmail.com>
2023-11-13 15:20:42 -0500

    Pod 153: Synchronize pod data

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,20 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+fn hello_cargo() {
+
+    // Printing values
+    let x: i32 = 5;
+    let y: i32 = 10;
+    println!("153: x = {x} and y + 2 = {}", y + 2);
+
+    // Random number, local to current thread
+    let random = rand::thread_rng()
+        .gen_range(1..=100);
+    println!("153: random value is: {random}");
+
+}
+
 fn main() {
 
     let secret: u32 = rand::thread_rng()
