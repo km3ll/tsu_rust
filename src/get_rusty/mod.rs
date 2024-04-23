5553e68deecdb2622509dae2e7b2678cfa608c6b
km3ll <kmell@protonmail.com>
2024-04-23 00:01:03 -0500

    Pod 153: Synchronize pod data

--- src/get_rusty/mod.rs
+++
@@ -1,3 +0,0 @@
-pub fn greet() {
-    println!("hello motto");
-}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
 // pod: disables the warning over unused function
 #![allow(unused)]
-mod rust_pl;
-mod get_rusty;
+mod m01_beginner;
 
-use crate::get_rusty::greet;
-use crate::rust_pl::c01_pod::start_pod;
-use crate::rust_pl::c02_game::start_game;
-use crate::rust_pl::c03_concepts::*;
+use crate::m01_beginner::*;
 
 fn main() {
-    // shadowing_02();
-    // shadowing_01();
-    // constants();
-    // mutability();
-    // start_game();
-    // start_pod();
-    // greet()
+    variables();
 }
