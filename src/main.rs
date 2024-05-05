d17c3e723e3e99b60d209f41e09fb6830a7f0d69
km3ll <kmell@protonmail.com>
2024-05-04 20:21:35 -0500

    Pod 153: Synchronize pod data

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #![allow(unused)]
-mod m01_beginner;
+mod beginner_m01_setup;
 mod rust_pl;
 
 //use crate::rust_pl::c01_pod::*;
-use crate::m01_beginner::c01_hello_world::*;
+use crate::beginner_m01_setup::c01_hello_world::*;
 
 fn main() {
     greet();
