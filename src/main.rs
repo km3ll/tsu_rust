815ca714ed25442841e05a861e852e4f07ef0f7c
km3ll <kmell@protonmail.com>
2024-05-05 23:48:34 -0500

    Pod 153: Synchronize pod data

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 #![allow(unused)]
 mod beginner_m01_setup;
+mod beginner_m02_memory;
 mod rust_pl;
 
-use beginner_m01_setup::c05_functions::run_fizz_buzz;
-
-//use crate::rust_pl::c01_pod::*;
-use crate::beginner_m01_setup::c06_control_flow::*;
+use beginner_m02_memory::c01_ownership::*;
 
 fn main() {
-    run_fizz_buzz();
+    clone_primitives();
 }
