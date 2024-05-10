49e128c1308e97ecb5c5121b130c2f5a442a32b0
kmell <kmell@protonmail.com>
2024-05-09 21:53:48 -0500

    Pod 153: Synchronize pod data

---
+++ bootcamp/01_beginner/02_memory/c01_ownership/src/main.rs
@@ -0,0 +1,32 @@
+#![allow(unused)]
+
+fn main() {
+    /*
+    {
+        let s1: String = String::from("Hello"); // Heap allocated
+        //Error: s1 is not valid outside the inner scope
+    } // s1 would be dropped here
+     */
+    let s1: String = String::from("Hello"); // Heap allocated
+    println!("Value of s1 is: {}", s1);
+} // si is dropped
+
+fn move_ownership() {
+    let s1: String = String::from("Hello"); // Heap allocated
+    let s2: String = s1;
+
+    //println!("Value of s1 is: {}", s1); // borrow of moved value
+    println!("Value of s2 is: {}", s2);
+}
+
+fn clone_ownership() {
+    let s1: String = String::from("Hello");
+    let s2: String = s1.clone();
+    println!("Value of s1 is: {s1}. Value of s2 is: {s2}    ");
+}
+
+fn clone_primitives() {
+    let x: i32 = 10;
+    let y: i32 = x;
+    println!("Value of y is: {y}");
+}
