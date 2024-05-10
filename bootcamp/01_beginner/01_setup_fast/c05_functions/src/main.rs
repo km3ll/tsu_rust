6d5aa1b94a7ebdbce94d8b8f1346af8b4c216f1c
kmell <kmell@protonmail.com>
2024-05-09 22:12:13 -0500

    Pod 153: Synchronize pod data

--- bootcamp/01_beginner/01_setup_fast/c05_functions/src/main.rs
+++ bootcamp/01_beginner/01_setup_fast/c05_functions/src/main.rs
@@ -7,11 +7,11 @@ fn main() {
     run_fizz_buzz();
 }
 
-pub fn print_age(age: i32) {
+fn print_age(age: i32) {
     println!("You are {} years old.", age);
 }
 
-pub fn get_age() -> i32 {
+fn get_age() -> i32 {
     let age: i32 = rand::thread_rng().gen_range(18..=80);
     age
 }
@@ -23,7 +23,7 @@ except for multiples of 3 it prints "fizz" instead of the number, and for
 multiples of 5 it prints "buzz" instead of the number. If the number is
 both a multiple of 3 and 5, it prints "fizzbuzz".
 */
-pub fn run_fizz_buzz() {
+fn run_fizz_buzz() {
     let mut n: i32 = 1;
     while n <= 100 {
         if n % 15 == 0 {
