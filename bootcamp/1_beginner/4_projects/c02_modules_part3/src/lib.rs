b4e88ae29a48cdd1fc288ee5d8eaa69536e26a44
kmell <kmell@protonmail.com>
2024-05-16 20:04:25 -0500

    Pod 153: Synchronize pod data

--- bootcamp/1_beginner/4_projects/c02_modules_part3/src/lib.rs
+++ bootcamp/1_beginner/4_projects/c02_modules_part3/src/lib.rs
@@ -1,3 +1,5 @@
+use rand::prelude::*;
+
 // The crate module has two sub-modules 
 mod database;
 mod auth_utils;
@@ -8,6 +10,9 @@ pub use auth_utils::models::Credentials;
 use database::Status;
 
 pub fn authenticate(creds: Credentials) {
+    let timeout = thread_rng().gen_range(100..500);
+    println!("The timeout is {timeout}");
+
     if let Status::Connected = database::connect_to_database() {
         auth_utils::login(creds);
         println!("[Authenticated!]");
