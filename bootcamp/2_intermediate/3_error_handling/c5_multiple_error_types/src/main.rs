e6f5db4feb4be8d9718ef0853e39b23350dfcca5
pod153 <kmell@protonmail.com>
2024-05-31 22:55:48 -0500

    Pod 153: Synchronize pod data

--- bootcamp/2_intermediate/3_error_handling/c5_multiple_error_types/src/main.rs
+++ bootcamp/2_intermediate/3_error_handling/c5_multiple_error_types/src/main.rs
@@ -1,3 +1,55 @@
+#![allow(unused)]
+
+use std::{fs, io, error, num::ParseIntError};
+
 fn main() {
-    println!("Hello, world!");
+    
+    println!("Parsing file...");
+    match parse_file_v2("example.txt") {
+        Ok(i) => println!("{i}"),
+        Err(e) => {
+            match e {
+                ParseFileError::File => {
+                    println!("Error parsing file");
+                },
+                ParseFileError::Parse(error) => {
+                    println!("Error parsing content: {}", error);
+                }
+            }
+        }
+    }
+
+}
+
+// Another approach is to create a custom error Enum with the variants of the 
+// different types of errors a function could return.
+enum ParseFileError {
+    File,
+    Parse(ParseIntError) // tupple varian
+}
+
+fn parse_file_v2(filename: &str) -> Result<i32, ParseFileError>{
+
+    // read_to_string and parse methods return different errors
+    let s = 
+        fs::read_to_string(filename)
+            .map_err(|e| ParseFileError::File)?;
+    let i =
+        s.parse()
+            .map_err(|e| ParseFileError::Parse(e))?;
+    Ok(i)
+
+}
+
+// The function could return any type that implements the error trait.
+// The downside is that callers of our functions won't know what concrete
+// error types are being returned at compile time, and therefore cannot 
+// handle individual errors differently.
+fn parse_file_v1(filename: &str) -> Result<i32, Box<dyn error::Error>>{
+
+    // read_to_string and parse methods return different errors
+    let s = fs::read_to_string(filename)?;
+    let i = s.parse()?;
+    Ok(i)
+
 }
