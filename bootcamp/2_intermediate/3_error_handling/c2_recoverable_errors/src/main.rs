f2ebc4a648e8bec2610b5fce4757e633d81a5920
pod153 <kmell@protonmail.com>
2024-05-31 21:29:08 -0500

    Pod 153: Synchronize pod data

--- bootcamp/2_intermediate/3_error_handling/c2_recoverable_errors/src/main.rs
+++ bootcamp/2_intermediate/3_error_handling/c2_recoverable_errors/src/main.rs
@@ -1,3 +1,40 @@
+#![allow(unused)]
+
+use std::{fs::File, io::Write};
+
 fn main() {
-    println!("Hello, world!");
+
+    match get_user_id("") {
+        Ok(id) => println!("Success: User id is {}", id),
+        Err(e) => println!("Error  : {}", e)
+    }
+
+    match get_user_id("johnwick") {
+        Ok(id) => println!("Success: User id is {}", id),
+        Err(e) => println!("Error  : {}", e)
+    }
+
+    let result1: Result<File, std::io::Error> = File::open("example.txt");
+    let file: File = match result1 {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("Failed to open file_1: {}", error);
+        }
+    };
+
+    // unwrap
+    let file_2: File = File::open("example.txt").unwrap();
+
+    // expect
+    let file_3: File = File::open("example2.txt")
+        .expect("Failed to open file_3!");
+
+}
+
+fn get_user_id(username: &str) -> Result<i32, String> {
+    if username.is_empty() {
+        Err("Username cannot be empty".to_owned())
+    } else {
+        Ok(1)
+    }
 }
