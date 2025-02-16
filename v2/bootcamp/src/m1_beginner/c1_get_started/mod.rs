7819d874bf79ffe31567650d928abba335b1e9e8
km3ll <kmell@protonmail.com>
2025-02-15 23:25:24 -0500

    Pod 153: Synchronize pod data

---
+++ v2/bootcamp/src/m1_beginner/c1_get_started/mod.rs
@@ -0,0 +1,1 @@
+pub mod u1_hello_world;
---
+++ v2/bootcamp/src/main.rs
@@ -0,0 +1,10 @@
+// Mute warnings
+#![allow(unused)]
+
+mod m1_beginner;
+
+use crate::m1_beginner::c1_get_started::u1_hello_world::*;
+
+fn main() {
+    greeting()
+}
