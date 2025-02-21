f45d7667156899ebda196f06f779d9116949dc07
km3ll <kmell@protonmail.com>
2025-02-20 22:55:39 -0500

    Pod 153: Synchronize pod data

--- v2/bootcamp/src/m1_beginner/c1_get_started/u2_variables/mod.rs
+++ v2/bootcamp/src/m1_beginner/c1_get_started/u2_variables/mod.rs
@@ -1,31 +1,31 @@
 pub fn variables() {
-    // creation
     println!("---");
     println!("Variables");
 
+    println!("creation");
     let a1: i16 = 5;
     let a2: f32 = 5.0;
     println!("a1: {a1}");
     println!("a2: {a2}");
 
-    // mutability
+    println!("mutability");
     let mut m1: i16 = 4;
     println!("m1: {m1}");
     m1 = 6;
     println!("m1: {m1}");
 
-    // shadowing
+    println!("shadowing");
     let s1: i32 = 10;
     let s1: i32 = 20;
     println!("s1: {s1}");
 
-    // scope
+    println!("scope");
     let d1: i16 = 40;
     {
         // This d1 lives within the scope of brackets {}
         let d1: i16 = 30;
-        println!("inner scope d1: {d1}");
+        println!("inner d1: {d1}");
     }
-    println!("outer scope d1: {d1}");
+    println!("outer d1: {d1}");
 
 }
