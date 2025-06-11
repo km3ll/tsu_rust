6cb4a944b3872d81bd3f6bdbc799c65f81d4f13b
km3ll <kmell@protonmail.com>
2025-06-10 22:59:00 -0500

    Pod 153: Synchronize pod data

---
+++ v2/the_book/src/main.rs
@@ -0,0 +1,15 @@
+#![allow(unused)]
+
+use crate::c01_started::m03_hello_cargo::hello_cargo;
+
+mod c01_started;
+
+fn main() {
+	chapter_01()
+}
+
+// 1. Getting Started
+fn chapter_01() {
+	// 1.3. Hello, Cargo!
+	hello_cargo();
+}
