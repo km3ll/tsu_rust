c6ad4217468d743e0891a57d73afeafeb60de7f1
km3ll <kmell@protonmail.com>
2025-06-11 23:08:38 -0500

    Pod 153: Synchronize pod data

--- v2/the_book/src/c02_guessing_game/c02u01_guessing_game.rs
+++ v2/the_book/src/c02_guessing_game/c02u01_guessing_game.rs
@@ -1,16 +1,31 @@
+/***
+pod: Prelude
+A set of items defined in the standard library 'std' that it brings
+into the scope of every program.
+ */
 use std::io;
 
 pub fn start_game() {
+	println!("Guess the number!");
+	println!("Please, input your guess:");
 
-    println!("Guess the number!");
-    println!("Please, input your guess:");
+	/***
+	pod: Mutability
+	In Rust, variables are immutable by default.
+	Add 'mut' before a variable name to make it mutable
+	 */
 
-    let mut guess: String = String::new();
+	/***
+	pod: Associated Function
+	The :: syntax in the ::new line indicates that new is an associated function
+	of the String type. An associated function is a function that’s implemented
+	on a type, in this case String.
+	 */
+	let mut guess: String = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+	io::stdin()
+		.read_line(&mut guess)
+		.expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
-
+	println!("You guessed: {}", guess);
 }
