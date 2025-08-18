f604f7b96cb6bdd582c71efacf3aa24bb1f09c2b
km3ll <kmell@protonmail.com>
2025-08-17 22:59:31 -0500

    Pod 153: Synchronize pod data

--- v2/the_book/src/main.rs
+++ v2/the_book/src/main.rs
@@ -3,7 +3,8 @@ use std::fs;
 use std::io::Result;
 use std::path::Path;
 use tsu::config::AppConfig;
-use crate::tsu::index::{load_file, Note};
+use crate::tsu::index::*;
+use std::collections::HashSet;
 
 // config: Module Import
 mod tsu;
@@ -11,13 +12,6 @@ mod tsu;
 fn main() {
 	let config: AppConfig = load();
 	start(config);
-	//load_note("resources/index/one-piece/note.txt");
-	//let lines = load_file("resources/index/tag/character.txt");
-	//for line in &lines {
-	//	println!("{}", line);
-	//}
-	//let note = Note::new(lines);
-
 }
 
 fn load() -> AppConfig {
@@ -27,13 +21,33 @@ fn load() -> AppConfig {
 fn start(config: AppConfig) {
 	// tags
 	let characters = load_file("resources/index/tag/character.txt");
+	let concepts = load_file("resources/index/tag/concept.txt");
+	let fruits = load_file("resources/index/tag/fruit.txt");
 	let places = load_file("resources/index/tag/place.txt");
-	let technique = load_file("resources/index/tag/technique.txt");
+	let techniques = load_file("resources/index/tag/technique.txt");
 
 	// note
 	let lines = load_file("resources/index/one-piece/note.txt");
 	let note = Note::new(lines);
-	for line in note.content {
-		println!("{}", line);
+	let candidates = list_candidates(&note.content);
+
+	let checked = uncommon_elements(characters, candidates);
+	let checked = uncommon_elements(places, checked);
+	let checked = uncommon_elements(techniques, checked);
+	let checked = uncommon_elements(fruits, checked);
+	let checked = uncommon_elements(concepts, checked);
+
+	println!("Uncategorized: ");
+	for i in checked {
+		println!(" - {}", i);
 	}
+}
+
+fn uncommon_elements(a: Vec<String>, b: Vec<String>) -> Vec<String> {
+	let set_a: HashSet<_> = a.into_iter().collect();
+	let set_b: HashSet<_> = b.into_iter().collect();
+	set_a
+		.symmetric_difference(&set_b) // items in A or B but not both
+		.cloned()                     // clone &String → String
+		.collect()
 }
--- v2/the_book/src/tsu/index/mod.rs
+++ v2/the_book/src/tsu/index/mod.rs
@@ -5,12 +5,12 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct Note {
-    pub content: Vec<String>,
+    pub content: String,
 }
 
 impl Note {
     pub fn new(lines: Vec<String>) -> Note {
-        let content = lines.into_iter()
+        let content: String = lines.into_iter()
             .filter_map(|line| if is_invalid_line(&line) { None } else { Some(line) })
             .collect();
         Note { content }
@@ -28,4 +28,31 @@ pub fn load_file(file: &str) -> Vec<String> {
     let content = fs::read_to_string(path)
         .expect(&format!("Something went wrong reading file: {}", file));
     content.lines().map(|line| line.to_string()).collect()
+}
+
+pub fn list_candidates(line: &String) -> Vec<String> {
+    let mut candidates: Vec<String> = line
+        .split("-")
+        .filter_map(|token| if is_candidate(token) { Some(token.trim().to_string()) } else { None })
+        .map(|token| format_candidate(&token))
+        .collect();
+    candidates.sort();
+    candidates.dedup();
+    candidates
+}
+
+pub fn is_candidate(token: &str) -> bool {
+    if (token.is_empty()) { return false; }
+    let trimmed = token.trim();
+    match trimmed.chars().next() {
+        Some(c) => c.is_uppercase(),
+        None => false
+    }
+}
+
+pub fn format_candidate(token: &str) -> String {
+    match token.split(":").next() {
+        Some(token) => token.to_string(),
+        None => "".to_string()
+    }
 }
