2f78ea487a7b41e295177b9a906a780b55d2e6f6
km3ll <kmell@protonmail.com>
2025-08-20 22:53:03 -0500

    Pod 153: Synchronize pod data

--- v2/app_base/src/main.rs
+++ v2/app_base/src/main.rs
@@ -2,10 +2,12 @@ mod tsu;
 
 use tsu::config::AppConfig;
 use tsu::resource::File;
+use tsu::serde::*;
 
 fn main() {
 	load_config();
 	load_file("resources/one-piece.txt");
+	use_serde();
 }
 
 fn load_config() {
@@ -16,8 +18,16 @@ fn load_config() {
 
 fn load_file(file: &str) {
 	println!("----");
+	println!("Lines:");
 	let file = File::from_file(file);
 	for line in file.lines {
-		println!("{}", line)
+		println!("- {}", line)
 	}
+}
+
+fn use_serde() {
+	println!("----");
+	serdes_json();
+	println!("----");
+	serdes_xml();
 }
