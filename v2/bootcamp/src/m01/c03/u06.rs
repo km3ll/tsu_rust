327763ded02047a9e7ca4ade7ed4d01c4092de7e
km3ll <kmell@protonmail.com>
2025-09-11 06:25:18 -0500

    Pod 153: Synchronize pod data

--- v2/bootcamp/src/m01/c03/u06.rs
+++ v2/bootcamp/src/m01/c03/u06.rs
@@ -1,5 +1,37 @@
-pub fn option_() {
-	println!("Base");
+fn find_name(id: u32) -> Option<String> {
+	let name = String::from("Ferris");
+	if (id == 1) {
+		Some(name)
+	} else {
+		None
+	}
+}
+
+pub fn option_enum() {
+	/**
+	 * pod: Option is defined and loaded in the prelude
+	 */
+	let rs: Option<String> = find_name(2);
+	match rs {
+		Some(name) => println!("name: {name}"),
+		None => println!("name not found!"),
+	}
+}
+
+pub fn option_if_let_syntax() {
+	/**
+	 * pod: Options if-let syntax
+	 * * - works when you only care about one variant
+	 */
+	let rs1 = find_name(1);
+	if let Some(name) = rs1 {
+		println!("name: {name}")
+	}
+
+	let rs2 = find_name(2);
+	if let None = rs2 {
+		println!("name not found!")
+	}
 }
 
 #[cfg(test)]
@@ -7,8 +39,14 @@ mod tests {
 	use super::*;
 
 	#[test]
-	fn run_() {
-		option_();
+	fn run_option_enum() {
+		option_enum();
+		assert!(true)
+	}
+
+	#[test]
+	fn run_option_if_let_syntax() {
+		option_if_let_syntax();
 		assert!(true)
 	}
 }
