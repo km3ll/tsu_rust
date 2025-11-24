f8143585a60cae8e44d8b12db8c192b9e2bac2aa
km3ll <kmell@protonmail.com>
2025-11-24 15:58:49 -0500

    Pod 153: Synchronize pod data

--- projects/bootcamp/src/m01/c01/u07_flow_control.rs
+++ projects/bootcamp/src/m01/c01/u07_flow_control.rs
@@ -1,13 +1,16 @@
+use rand::Rng;
+
 #[allow(unused)]
 
 pub fn flow_ifelse() {
-	let a: i8 = 25;
-	if a > 30 {
-		// println!("Bigger than 30");
-	} else if a > 20 {
-		// println!("Bigger than 20");
+	let a1 = rand::rng()
+		.random_range(-40..40);
+	if a1 > 30 {
+		println!("ifelse: Bigger than 30");
+	} else if 1 > 20 {
+		println!("ifelse: Bigger than 20");
 	} else {
-		// println!("Smaller or equal to 20")
+		println!("ifelse: Smaller or equal to 20")
 	}
 }
 
@@ -18,62 +21,60 @@ pub fn flow_ifelse_in_let() {
 	---"#;
 	println!("{n1}");
 
-	let a: i8 = 25;
-	let b: i8 = if a > 18 { 1 } else { -1 };
+	let a1: i8 = 25;
+	let b1: i8 = if a1 > 18 { 1 } else { -1 };
+	println!("let expression b1: {b1}")
 }
 
 pub fn flow_loop() {
 	let n1 = r#"
-	pod: Loop 'break'
-	- Exits a loop
+	pod: Loops
+	- loop
+	  - inner / outer
+	  - break
+	  - break 'name (name starts with tick ('))
+	  - brear value
+	- while
+	- for
 	---"#;
 	println!("{n1}");
 
 	loop {
+		println!("loop: break");
 		break;
 	}
 }
 
 pub fn flow_labeling_loops() {
-	let n1 = r#"
-	pod: Labeling loops
-	- The name must start with a tick (')
-	---"#;
-	println!("{n1}");
-
-	'outer: loop {
-		println!("outer loop");
+	println!("outer loop");
+	'name: loop {
+		println!("inner loop");
 		loop {
-			println!("inner loop");
-			break 'outer;
+			println!("loop: break 'name");
+			break 'name;
 		}
 	}
 }
 
 pub fn flow_loop_returning_value() {
-	let n1 = r#"
-	pod: Loop 'break' with value
-	---"#;
-	println!("{n1}");
-
 	let x: i8 = loop {
-		// println!("loop returning 5");
+		println!("loop: break 5");
 		break 5;
 	};
 }
 
 pub fn flow_while_loop() {
-	let mut count: i8 = 1;
-	while count <= 5 {
-		// println!("count: {count}");
-		count = count + 1;
+	let mut i: i8 = 1;
+	while i <= 5 {
+		println!("while-loop i: {i}");
+		i = i + 1;
 	}
 }
 
 pub fn flow_for_loop() {
-	let numbers: [i8; 4] = [10, 20, 30, 40];
-	for element in numbers {
-		// println!("element: {}", element)
+	let elements: [i8; 4] = [10, 20, 30, 40];
+	for e in elements {
+		println!("for-loop e: {e}")
 	}
 }
 
