9134c64513d8287eb7cd24cbbde486c3aaf7bd10
km3ll <kmell@protonmail.com>
2025-11-25 15:26:14 -0500

    Pod 153: Synchronize pod data

--- projects/bootcamp/src/m01/c02/u04_borrowing.rs
+++ projects/bootcamp/src/m01/c02/u04_borrowing.rs
@@ -1,37 +1,42 @@
 fn print_string(r1: &String) {
-	// println!("r1: {r1}")
+	println!("Borrowing: r1: {r1}");
+}
+
+fn add_to_string(r2: &mut String) {
+	println!("Borrowing adding to reference: r2: {r2}");
+	r2.push_str(" is awesome!");
 }
 
 pub fn borrowing_definition() {
 	let n1 = r#"
 	pod: Borrowing
 	- The act of creating a reference
-	- References are pointers with rules/restrictions
-	- References do not take ownership
-	Why borrow?
-	- Performance
-	- When ownership is not needed/desired
+	- Why borrow? For performance or when ownership is not needed/desired
+	---
+	pod: References
+	- Are pointers with rules/restrictions
+	- Do not take ownership
+	- Are created with ampersand (&)
+	- Can be mutable/immutable
+	- To declare a mutable reference the variable itself has to be mutable
+	---
+	pod: Borrowing Rules
+	- At any given time, you can have either one mutable reference or any number of immutable references
+	- References must always be valid
 	---"#;
 	println!("{n1}");
-
-	let n2 = r#"
-	pod: Borrowing Rules
-	- At any given time, you can have either one mutable reference
-	  or any number of immutable references
-	- References must always be valid (created with &)
-	---"#;
-	println!("{n2}");
 }
 
 pub fn borrowing_immutable_reference() {
 	let n1 = r#"
-	pod: References
-	- Can be mutable or immutable
+	pod: Borrowing Mutable/Immutable References
+	- When mutable, the borrowed type must also be mutable
 	---"#;
 	println!("{n1}");
 
 	let s1: String = String::from("Ferris");
 	let r1: &String = &s1;
+	println!("Immutable reference: r1: {r1}");
 	print_string(r1);
 }
 
@@ -39,23 +44,13 @@ pub fn borrowing_mutable_reference() {
 	let n1 = r#"
 	pod: Automatic Dereferencing
 	- We don't need to dereference r2
-	- The asterisk '*' is a dereference operator
-	  (*r2).push_str(" is awesome!");
-	---"#;
-	println!("{n1}");
-	fn add_to_string(r2: &mut String) {
-		r2.push_str(" is awesome!");
-	}
-
-	let n1 = r#"
-	pod: Mutable Reference
-	- In order to declare a mutable reference the variable itself
-	  has to be mutable
+	- The asterisk '*' is a dereference operator (*r2).push_str(" is awesome!");
 	---"#;
 	println!("{n1}");
 
 	let mut s2: String = String::from("Ferris");
 	let r2: &mut String = &mut s2;
+	println!("Mutable reference: r2: {r2}");
 	add_to_string(r2);
 }
 
@@ -70,6 +65,9 @@ pub fn borrowing_dangling_reference() {
 	 */
 	let n1 = r#"
 	pod: Dangling Reference
+	- A reference that points to memory that has already been freed, moved or gone out of scope
+	- The referenced data is no longer valid
+	- Rust prevents this at compile time
 	---"#;
 	println!("{n1}");
 }
