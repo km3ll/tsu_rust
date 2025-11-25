dd4e14360b4f6074597a5039062cfa8480160320
km3ll <kmell@protonmail.com>
2025-11-25 15:14:26 -0500

    Pod 153: Synchronize pod data

--- projects/bootcamp/src/m01/c02/u03_ownership_part_2.rs
+++ projects/bootcamp/src/m01/c02/u03_ownership_part_2.rs
@@ -1,21 +1,28 @@
 fn print_string(p1: String) {
-	// println!("p1: {p1}")
+	println!("Ownership moved: p1: {p1}")
 } // p1 is dropped here
 
 fn generate_string() -> String {
-	String::from("Ferris")
+	let p1 = String::from("Ferris");
+	println!("Ownership returned: p1: {p1}");
+	p1
 }
 
 fn print_integer(i1: i32) {
-	// println!("i1: {i1}");
+	println!("Ownership printed: i1: {i1}");
+}
+
+fn add_to_string(mut p1: String) -> String {
+	p1.push_str(" is awesome!");
+	println!("Appending to p1: {p1}");
+	p1
 }
 
 pub fn ownership_pass_into_function() {
 	let n1 = r#"
-	pod: Moving ownership INTO functions
+	pod: Ownership moved INTO functions
 	- Values are moved by default
-	- Passing a variable into a function has the same effect
-	  as assigning one variable to another variable
+	- Passing a variable into a function has the same effect as assigning one variable to another variable
 	---"#;
 	println!("{n1}");
 
@@ -26,9 +33,8 @@ pub fn ownership_pass_into_function() {
 
 pub fn ownership_clone_into_function() {
 	let n1 = r#"
-	pod: Cloning before calling a function
-	- Instead of moving ownership, we first cloine s2 and then
-	  move ownership of that clone into the function
+	pod: Ownership cloned before calling functions
+	- We first clone s2 and then move ownership of that clone into the function
 	---"#;
 	println!("{n1}");
 
@@ -38,40 +44,31 @@ pub fn ownership_clone_into_function() {
 
 pub fn ownership_out_of_function() {
 	let n1 = r#"
-	pod: Moving ownership OUT of functions
-	- Ownership of the generated function is transferred to s3
+	pod: Ownership moved OUT of functions
+	- Ownership of the generated function is transferred to s3 (caller)
 	---"#;
 	println!("{n1}");
 
 	let s3: String = generate_string();
+	println!("Ownership from function: s3: {s3}");
 }
 
 pub fn ownership_in_and_out_functions() {
 	let n1 = r#"
-	pod: Append to String
-	- 'push_str' method
-	---"#;
-	println!("{n1}");
-
-	fn add_to_string(mut p1: String) -> String {
-		p1.push_str(" is awesome!");
-		p1
-	}
-
-	let n1 = r#"
-	pod: Functions taking ownership and giving it back
+	pod: Ownership INTO and OUT of functions
+	- Functions taking ownership and giving it back (push_str)
 	---"#;
 	println!("{n1}");
 
 	let s1: String = String::from("Ferris");
 	let s2 = add_to_string(s1);
+	println!("Ownership INTO/OUT: s2: {s2}");
 }
 
 pub fn ownership_stack_datatypes() {
 	let n1 = r#"
 	pod: Stack-Only data types
-	- The value of x was cloned into z
-	- The value of x was also cloned into i
+	- The value of x: i32 was cloned into z
 	---"#;
 	println!("{n1}");
 
