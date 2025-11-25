d92b649dafd95ef7a7e4775fb5cc8211e3941d65
km3ll <kmell@protonmail.com>
2025-11-25 15:45:27 -0500

    Pod 153: Synchronize pod data

--- projects/bootcamp/src/m01/c02/u05_slices.rs
+++ projects/bootcamp/src/m01/c02/u05_slices.rs
@@ -1,3 +1,13 @@
+fn trim_tweet(tweet: &String) -> &str {
+	println!("Slices: trimming tweet: {tweet}");
+	&tweet[..16]
+}
+
+fn trim_tweet_deref(tweet: &str) -> &str {
+	println!("Slices: trimming deref tweet: {tweet}");
+	&tweet[..16]
+}
+
 pub fn slices_range_syntax() {
 	let n1 = r#"
 	pod: Slices
@@ -10,68 +20,70 @@ pub fn slices_range_syntax() {
 	let s1: &str = &tweet[..20]; // beginning
 	let s2: &str = &tweet[20..]; // ending
 	let s3: &str = &tweet[..]; // entire
+	println!("Slices: s2: '{s2}', s1: '{s1}'");
 }
 
 pub fn slices_string_types() {
 	let n1 = r#"
 	pod: String
-	- Growable, heap allocated string (UTF-8 encoded)
+	- Growable, UTF-8 encoded, heap-allocated value
 	---"#;
 	println!("{n1}");
 
 	let tweet: String = String::from("This is my tweet and it's very long.");
-	let t1: &str = &tweet[..20];
+	let s1: &str = &tweet[..20];
+	println!("Slices: s1: {s1}");
 }
 
 pub fn slices_string_literal() {
 	let n1 = r#"
-	pod: String slice '&str'
-	- Immutable sequence of UTF-8 bytes somewhere in memory
-	  (stack, heap, or static memory)
-	- Handle behind a reference (&str) because length of
-	  sequence is unknown at compile time
+	pod: String Slice
+	- Immutable sequence of UTF-8 bytes somewhere in memory (stack, heap, or static)
+	- Handled behind a reference (&str) because length of sequence is unknown at compile time
 	- All string literals are string slices
 	---"#;
 	println!("{n1}");
 
 	let s4: &str = "Ferris the crab";
+	println!("Slices, string literal: {s4}");
 }
 
 pub fn slices_string_slice_and_functions() {
-	fn trim_tweet(tweet: &String) -> &str {
-		&tweet[..16]
-	}
 	let tweet: String = String::from("This is my tweet and it's very long.");
 	let s5: &str = trim_tweet(&tweet);
+	println!("String slice from string reference: s5: {s5}");
 }
 
-pub fn slices_dref_coercion() {
+pub fn slices_deref_coercion() {
 	let n1 = r#"
-	pod: DRef Coercion
+	pod: Deref Coercion
+	- An automatic conversion the compiler performs when a type implements the Deref trait
+	- Allows a type to be implicitly treated as a reference to its target type
 	- Parameter type '&str' works for both: reference to string and string slice
+	- It exists to make smart-pointer types (Box, Arc, String) behave like normal references
 	---"#;
 	println!("{n1}");
 
-	fn trim_tweet(tweet: &str) -> &str {
-		&tweet[..16]
-	}
-
 	let tweet: String = String::from("This is my tweet and it's very long.");
-	let s6: &str = trim_tweet(&tweet);
-	let s7: &str = trim_tweet("This is my tweet and it's very long.");
+	let s1: &str = "This is another tweet.";
+	let s6: &str = trim_tweet_deref(&tweet);
+	let s7: &str = trim_tweet_deref(s1);
+	println!("Deref slices: s6: {s6}, s7: {s7}");
 }
 
 pub fn slices_vector_slices() {
 	let n1 = r#"
 	pod: Debug Formatting
+	- The standardized, developer-focused way to print a value for inspection
+	- A type supports it by implementing the std::fmt::Debug trait
 	- {:?} syntax
 	---"#;
 	println!("{n1}");
 
 	let v1: [i32; 6] = [1, 2, 3, 4, 5, 6];
 	let v2: &[i32] = &v1[..3];
-	// println!("{:?}", v1);
-	// println!("{:?}", v2);
+	println!("Debug format: s1: {:?}", v1);
+	println!("Debug format: s2: {:?}", v2);
 }
 
 #[cfg(test)]
@@ -99,8 +111,8 @@ mod tests {
 	}
 
 	#[test]
-	fn run_slices_dref_cohersion() {
-		slices_dref_coercion();
+	fn run_slices_deref_cohersion() {
+		slices_deref_coercion();
 	}
 
 	#[test]
