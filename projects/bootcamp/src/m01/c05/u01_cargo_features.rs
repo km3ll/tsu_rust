2cc540f9efb74ecf08c056aa3d7bd3c5b43542ab
km3ll <kmell@protonmail.com>
2025-11-27 22:03:23 -0500

    Pod 153: Synchronize pod data

--- projects/bootcamp/src/m01/c05/u01_cargo_features.rs
+++ projects/bootcamp/src/m01/c05/u01_cargo_features.rs
@@ -1,22 +1,16 @@
-pub fn features_definition() {
-	/**
-	 * pod: Cargo features
-	 * - Allow to define part of code that are conditionally compiled,
-	 *   only if a certain feature is enabled.
-	 * - Allow to define optional dependencies
-	 * - Reduce compile time and file sizes
-	 * - Disabled by default
-	 */
-	println!("cargo features");
-}
-
-pub fn features_cargo() {
-	/**
-	 * pod: Cargo.toml
-	 * - Features have a name and an associated array: other features or optional dependencies.
-	 * - Dependencies must be marked as optional
-	 */
-	println!("Cargo.toml");
+pub fn features() {
+	let n1 = r#"
+	pod: Cargo features
+	- Allow to define part of code that are conditionally compiled, only if a certain feature is enabled
+	- Allow to define optional dependencies
+	- Reduce compile time and file sizes
+	- Disabled by default
+	---
+	pod: Cargo.toml
+	- Features have a name and an associated array: other features or optional dependencies
+	- Dependencies must be marked as optional
+	---"#;
+	println!("{n1}");
 }
 
 #[cfg(test)]
@@ -24,12 +18,7 @@ mod tests {
 	use super::*;
 
 	#[test]
-	fn run_features_definition() {
-		features_definition();
-	}
-
-	#[test]
-	fn run_features_cargo() {
-		features_cargo();
+	fn run_features() {
+		features();
 	}
 }
--- projects/bootcamp/src/m01/c05/u02_cargo_workspaces.rs
+++ projects/bootcamp/src/m01/c05/u02_cargo_workspaces.rs
@@ -1,12 +1,12 @@
-pub fn workspace_virtual_manifest() {
-	/**
-	 * pod: Workspace
-	 * - A collection of related cargo packages.
-	 * - Packages in a workspace share a Cargo.lock file, meaning that if multiple
-	 *   packages have the same dependency, it will be resolved to one version.
-	 * - Packages in a workspace are called 'members' and they share one output directory.
-	 */
-	println!("workspace");
+pub fn workspaces() {
+	let n1 = r#"
+	pod: Workspace
+	- A collection of related cargo packages
+	- Packages in a workspace share a Cargo.lock file
+	- If multiple packages have the same dependency, it will be resolved to one version
+	- Packages in a workspace are called 'members' and they share one output directory
+	---"#;
+	println!("{n1}");
 }
 
 #[cfg(test)]
@@ -14,7 +14,7 @@ mod tests {
 	use super::*;
 
 	#[test]
-	fn run_workspace_virtual_manifest() {
-		workspace_virtual_manifest();
+	fn run_workspaces() {
+		workspaces();
 	}
 }
