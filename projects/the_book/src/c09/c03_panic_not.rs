7dc23b1302d0855150ca0d889dc0d49a59d61a01
km3ll <kmell@protonmail.com>
2025-12-10 22:27:56 -0500

    Pod 153: Synchronize pod data

---
+++ projects/the_book/src/c09/c03_panic_not.rs
@@ -0,0 +1,17 @@
+fn panic_not_() {
+	let n1 = r#"
+	pod:
+	-
+	---"#;
+	println!("{n1}");
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn run_() {
+		panic_not_();
+	}
+}
--- projects/the_book/src/c09/mod.rs
+++ projects/the_book/src/c09/mod.rs
@@ -1,2 +1,3 @@
 pub mod c01_unrecoverable_errors;
 pub mod c02_recoverable_errors;
+pub mod c03_panic_not;
